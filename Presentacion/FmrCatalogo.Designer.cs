a880a28a9ccd25f6f8541d67088f5ed074bc2e9c
Alan Galvan <alan_galvan97@hotmail.com>
2023-02-08 02:39:40 -0300

    cuarta subida

--- Presentacion/FmrCatalogo.Designer.cs
+++ Presentacion/FmrCatalogo.Designer.cs
@@ -31,16 +31,19 @@ namespace Presentacion
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmrCatalogo));
             this.panelArriba = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.ptbCerrar = new System.Windows.Forms.PictureBox();
             this.ptbRestaurar = new System.Windows.Forms.PictureBox();
             this.ptbMinimizar = new System.Windows.Forms.PictureBox();
             this.ptbMaximizar = new System.Windows.Forms.PictureBox();
+            this.ptbCerrar = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelArriba.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ptbCerrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbRestaurar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbMinimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbMaximizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbCerrar)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelArriba
@@ -53,36 +56,16 @@ namespace Presentacion
             this.panelArriba.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelArriba.Location = new System.Drawing.Point(0, 0);
             this.panelArriba.Name = "panelArriba";
-            this.panelArriba.Size = new System.Drawing.Size(1300, 58);
+            this.panelArriba.Size = new System.Drawing.Size(1015, 58);
             this.panelArriba.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 58);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 592);
-            this.panel1.TabIndex = 1;
-            // 
-            // ptbCerrar
-            // 
-            this.ptbCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.ptbCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ptbCerrar.Image = ((System.Drawing.Image)(resources.GetObject("ptbCerrar.Image")));
-            this.ptbCerrar.Location = new System.Drawing.Point(1267, 0);
-            this.ptbCerrar.Name = "ptbCerrar";
-            this.ptbCerrar.Size = new System.Drawing.Size(33, 33);
-            this.ptbCerrar.TabIndex = 2;
-            this.ptbCerrar.TabStop = false;
-            this.ptbCerrar.Click += new System.EventHandler(this.ptbCerrar_Click);
+            this.panelArriba.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelArriba_MouseDown);
             // 
             // ptbRestaurar
             // 
             this.ptbRestaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.ptbRestaurar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ptbRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("ptbRestaurar.Image")));
-            this.ptbRestaurar.Location = new System.Drawing.Point(1228, 0);
+            this.ptbRestaurar.Location = new System.Drawing.Point(943, 0);
             this.ptbRestaurar.Name = "ptbRestaurar";
             this.ptbRestaurar.Size = new System.Drawing.Size(33, 33);
             this.ptbRestaurar.TabIndex = 3;
@@ -95,7 +78,7 @@ namespace Presentacion
             this.ptbMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.ptbMinimizar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ptbMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("ptbMinimizar.Image")));
-            this.ptbMinimizar.Location = new System.Drawing.Point(1189, 0);
+            this.ptbMinimizar.Location = new System.Drawing.Point(904, 0);
             this.ptbMinimizar.Name = "ptbMinimizar";
             this.ptbMinimizar.Size = new System.Drawing.Size(33, 33);
             this.ptbMinimizar.TabIndex = 4;
@@ -107,29 +90,65 @@ namespace Presentacion
             this.ptbMaximizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.ptbMaximizar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ptbMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("ptbMaximizar.Image")));
-            this.ptbMaximizar.Location = new System.Drawing.Point(1228, 0);
+            this.ptbMaximizar.Location = new System.Drawing.Point(943, 0);
             this.ptbMaximizar.Name = "ptbMaximizar";
             this.ptbMaximizar.Size = new System.Drawing.Size(33, 33);
             this.ptbMaximizar.TabIndex = 5;
             this.ptbMaximizar.TabStop = false;
             this.ptbMaximizar.Click += new System.EventHandler(this.ptbMaximizar_Click);
             // 
+            // ptbCerrar
+            // 
+            this.ptbCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptbCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ptbCerrar.Image = ((System.Drawing.Image)(resources.GetObject("ptbCerrar.Image")));
+            this.ptbCerrar.Location = new System.Drawing.Point(982, 0);
+            this.ptbCerrar.Name = "ptbCerrar";
+            this.ptbCerrar.Size = new System.Drawing.Size(33, 33);
+            this.ptbCerrar.TabIndex = 2;
+            this.ptbCerrar.TabStop = false;
+            this.ptbCerrar.Click += new System.EventHandler(this.ptbCerrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 519);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // FmrCatalogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 650);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(68)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(1015, 577);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelArriba);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FmrCatalogo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Catalogo";
+            this.Load += new System.EventHandler(this.FmrCatalogo_Load);
             this.panelArriba.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.ptbCerrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbRestaurar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbMinimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbMaximizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbCerrar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -142,5 +161,6 @@ namespace Presentacion
         private System.Windows.Forms.PictureBox ptbMaximizar;
         private System.Windows.Forms.PictureBox ptbCerrar;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Presentacion/FmrCatalogo.cs
+++ Presentacion/FmrCatalogo.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using dominio;
 using negocio;
+using System.Runtime.InteropServices;
 
 namespace Presentacion
 {
@@ -42,6 +43,23 @@ namespace Presentacion
         private void ptbMinimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void FmrCatalogo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panelArriba_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
